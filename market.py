95c7e14bec6f9d4a74ad188317fee56eade0a0db
Daniel Lee <dlee0528@hotmail.com>
2021-08-08 16:53:18 -0700

    set up mySQL

--- market.py
+++ market.py
@@ -1,14 +1,28 @@
+from enum import unique
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+db = SQLAlchemy(app)
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    barcode = db.Column(db.Integer(), nullable=False)
+    price = db.Column(db.String(length=20), nullable=False, unique=True)
+    description = db.Column(db.String(length=1024), nullable=False, unique=True)
+
+    def __repr__(self) -> str:
+        return f'Item {self.name}'
+
+
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
-
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>This is about page of {username}</h1>'
 
 @app.route('/market')
 def market_page():
