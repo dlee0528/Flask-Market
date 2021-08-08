d16a98239ec317ec930995fe4c1c3d6f6d036693
Daniel Lee <dlee0528@hotmail.com>
2021-08-08 15:47:24 -0700

    add market.html with JINJA

--- market.py
+++ market.py
@@ -2,12 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home_page():
     return render_template('home.html')
 
 @app.route('/about/<username>')
 def about_page(username):
     return f'<h1>This is about page of {username}</h1>'
+
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='Phone')
 
 
 if __name__ == "__main__":
